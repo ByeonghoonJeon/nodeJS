f097d89524c235aee08f2b0438123d9e752146cd
Nathan Jeon <jeon4972@snu.ac.kr>
2021-10-20 16:12:29 -0400

    Practice

--- nucampsiteServer/app.js
+++ nucampsiteServer/app.js
@@ -13,6 +13,19 @@ const promotionRouter = require("./routes/promotionRouter");
 const partnerRouter = require("./routes/partnerRouter");
 
 const app = express();
+app.all("*", (req, res, next) => {
+  if (req.secure) {
+    return next();
+  } else {
+    console.log(
+      `Redirecting to: https://${req.hostname}:${app.get("secPort")}${req.url}`
+    );
+    res.redirect(
+      301,
+      `https://${req.hostname}:${app.get("secPort")}${req.url}`
+    );
+  }
+});
 
 const mongoose = require("mongoose");
 const url = config.mongoUrl;
